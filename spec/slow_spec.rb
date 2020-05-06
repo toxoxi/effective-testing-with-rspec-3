8d73a909313b9e416573d49236d62ccf709b3e1c
toxoxi <yorori.too@gmail.com>
2020-05-06 19:17:55 +0900

    how to use: rspec --profile n

---
+++ spec/slow_spec.rb
@@ -0,0 +1,9 @@
+# rspec --profile 2
+# →→→ Top 2 slowest examples ...
+RSpec.describe 'The sleep() method' do
+  it('can sleep for 0.1 second') { sleep 0.1 }
+  it('can sleep for 0.2 second') { sleep 0.2 }
+  it('can sleep for 0.3 second') { sleep 0.3 }
+  it('can sleep for 0.4 second') { sleep 0.4 }
+  it('can sleep for 0.5 second') { sleep 0.5 }
+end
