8b370532384849cae942e93306244f7c3f81d218
toxoxi <yorori.too@gmail.com>
2020-05-06 14:44:12 +0900

    how to use: before

--- spec/sandwich_spec.rb
+++ spec/sandwich_spec.rb
@@ -1,11 +1,18 @@
 Sandwich = Struct.new(:taste, :toppings)
 
 RSpec.describe 'An ideal sandwich' do
-  it 'is delicious' do
-    sandwich = Sandwich.new('delicious', [])
+  before { @sandwich = Sandwich.new('delicious', []) }
 
-    taste = sandwich.taste
+  it 'is delicious' do
+    taste = @sandwich.taste
 
     expect(taste).to eq('delicious')
+  end
+
+  it 'lets me add toppings' do
+    @sandwich.toppings << 'cheese'
+    toppings = @sandwich.toppings
+
+    expect(toppings).not_to be_empty
   end
 end
